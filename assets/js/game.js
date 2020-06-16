3736cb759e1b29cec6e1a803a9b48343e7f9e345
gminteer <gminteer@gmail.com>
2020-06-16 15:23:46 -0400

    minimum viable product

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,66 +4,135 @@
         - defeat each robot
     LOSE - player has lost all health
 */
-
-var playerName = window.prompt('What is your robot\'s name?');  /* I guess it's like Visual Basic class 20 years ago and this is "easier" than starting off 
-                                                                doing it the right way. Also: "var" is kind of pointless here since we didn't turn on mandatory
-                                                                variable declarations and it's not in a function so it's going to be a global anyway. It's also
-                                                                at the top of the file so hoisting isn't going to change anything. */
-var playerHealth = 100;
-var playerAttack = 10;
-var playerMoney = 10;
-
-console.log(playerName, playerHealth, playerAttack);
-
-var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-var enemyHealth = 50;
-var enemyAttack = 12;
+var playerName;
+var playerHealth;
+var playerAttack;
+var playerMoney;
+var enemyHealth;
+var enemyAttack;
 
 var fight = function(enemyName) {
-    // Announce the start of the round
-    window.alert('Welcome to Robot Gladiators!');
-    
     // fight or skip?
-    var promptFight;
-    while((promptFight != 'FIGHT') && (promptFight != 'SKIP')) {
-        promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.').toUpperCase();
+    var promptFight = 'blah';
+    while((promptFight[0] != 'F') && (promptFight[0] != 'S')) {
+        promptFight = window.prompt('Would you like to (F)IGHT or (S)KIP this battle? Enter "FIGHT"/"F" or "SKIP"/"S" to choose.').toUpperCase();
     }
-    if(promptFight == 'FIGHT') {
-        // Subract playerAttack from enemyHealth, updating enemyHealth w/ new value
-        enemyHealth -= playerAttack;
+    if(promptFight[0] == 'F') {
+        while(enemyHealth > 0 && playerHealth > 0) {
+            // Subract playerAttack from enemyHealth, updating enemyHealth w/ new value
+            enemyHealth -= playerAttack;
 
-        // has it lost all its health?
-        if(enemyHealth <= 0) {
-            window.alert(enemyName + ' has been defeated!');
-        } else {
-            window.alert(enemyName + ' has ' + enemyHealth + ' HP left.');
-        };
+            // has it lost all its health?
+            if(enemyHealth <= 0) {
+                window.alert(enemyName + ' has been defeated!');
+                break;
+            } else {
+                window.alert(enemyName + ' has ' + enemyHealth + ' HP left.');
+            }
 
-        // Console log
-        console.log(enemyName + ' attacked by ' + playerName + ', new health: ' + enemyHealth);
+            // Console log
+            console.log(enemyName + ' attacked by ' + playerName + ', new health: ' + enemyHealth);
 
-        // The enemy robot swings back
-        playerHealth -= enemyAttack;
+            // The enemy robot swings back
+            playerHealth -= enemyAttack;
 
-        // More logging
-        console.log(playerName + ' attacked by ' + enemyName + ', new health: ' + playerHealth);
+            // More logging
+            console.log(playerName + ' attacked by ' + enemyName + ', new health: ' + playerHealth);
 
-        // has it lost all its health?
-        if(playerHealth <= 0) {
-            window.alert(playerName + ' has been defeated!');
-        } else {
-            window.alert(playerName + ' has ' + playerHealth + ' HP left.');
-        };
-    } else if(promptFight == 'SKIP') {
-        var confirmSkip = window.confirm('Are you sure you\'d like to skip the fight?');
-        if(confirmSkip) {
-            window.alert(playerName + ' has skipped this fight.');
-            playerMoney -= 2;
-        } else {
-            fight(); // I wonder how many times you'd have to pick "skip" then "no" before we get a stack overflow...
+            // has it lost all its health?
+            if(playerHealth <= 0) {
+                window.alert(playerName + ' has been defeated!');
+                break;
+            } else {
+                window.alert(playerName + ' has ' + playerHealth + ' HP left.');
+            }
         }
-    };
+    } else if(promptFight[0] == 'S') {
+        var confirmSkip = window.confirm('Are you sure you\'d like to skip the fight?');
+        if(confirmSkip && (playerMoney >= 2)) {
+            window.alert(playerName + ' has skipped this fight for $2.');
+            playerMoney -= 2;
+            console.log('playerMoney: ' + playerMoney);
+        } else if(playerMoney < 2) {
+            window.alert('Not enough cash!');
+            fight(enemyName);
+        } else {
+            fight(enemyName); // I wonder how many times you'd have to pick "skip" then "no" before we get a stack overflow...
+        }
+    }
 };
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+
+var shop = function () {
+    var promptShop = 'blah';
+    while(true) {
+        promptShop = window.prompt('Cash left: ' + playerMoney + '. Would you like to (R)EFILL your health, (U)PGRADE your attack, or (L)EAVE the shop?').toUpperCase();
+        console.info(promptShop, promptShop[0]);
+        if(promptShop[0] == 'R') { //refill
+            if((playerMoney >= 2) && (playerHealth < 100)) {
+                playerMoney -= 2;
+                playerHealth = 100;
+            } else if(playerMoney < 2) {
+                window.alert('Not enough cash!');
+            } else if(playerHealth >= 100) {
+                window.alert('Already at max health!');
+            }
+        } else if(promptShop[0] == 'U') { //upgrade
+            if(playerMoney >= 2) {
+                playerMoney -=2;
+                playerAttack += 10;
+                window.alert('Attack power now: ' + playerAttack);
+            } else {
+                window.alert('Not enough cash!');
+            }
+        } else if(promptShop[0] == 'L') { //leave
+            if(window.confirm('Are you sure you want to leave?')) {
+                break;
+            }
+        }
+    }
 };
+
+var endGame = function() {
+    if(playerHealth > 0) {
+        window.alert('Your robot lives! Final score: ' + playerMoney);
+    }
+    if(window.confirm('Play again?')) {
+        return true;
+    }
+    return false;
+};
+
+var startGame = function() {
+    //playerData
+    playerName = window.prompt('What is your robot\'s name?');
+    playerHealth = 100;
+    playerAttack = 10;
+    playerMoney = 10;
+    //enemyData
+    var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
+    enemyHealth = 50;
+    enemyAttack = 12;
+    //fight the enemies
+    for(var i = 0; i < enemyNames.length; i++) {
+        // Announce the start of the round
+        if(playerHealth > 0) {
+            window.alert('Welcome to Robot Gladiators! Round: ' + (i + 1));
+        } else {
+            break;
+        }
+        var pickedEnemyName = enemyNames[i];
+        enemyHealth = 50;
+        fight(pickedEnemyName);
+        if(i < (enemyNames.length - 1)) {
+            if(window.confirm('Visit shop?')) {
+                shop();
+            }
+        }
+    }
+};
+
+gameLoop = true;
+while(gameLoop) {
+    startGame();
+    gameLoop = endGame();
+}
